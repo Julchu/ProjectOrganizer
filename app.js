2c8fcfbbecce9cd1458f43f9f4a9acf0d19d5dab
Julian <jchumtl@gmail.com>
2017-09-25 11:30:43 -0400

    Working displaying frontend from backend

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
 var login = require('./routes/login');
-var users = require('./routes/users');
+var project = require('./routes/project');
 
 var app = express();
 
@@ -23,9 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/index', index);
-app.use('/login', login);
-app.use('/users', users);
+app.use('/', login);
+app.use('/project', project);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
