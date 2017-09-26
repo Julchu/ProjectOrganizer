f98cfc3ab38c3adb1eee92520d449a917f2eaac7
Julian Chu <chux9490@mylaurier.ca>
2017-09-26 18:12:58 -0400

    Trying to fix MySQL issues

--- routes/sql.js
+++ routes/sql.js
@@ -4,26 +4,43 @@ var mysql = require('mysql');
 
 var router = express.Router();
 
-var db = mysql.createConnection({
-    host     : 'us-cdbr-iron-east-05.cleardb.net',
-    user     : 'b4a8e285a022ee',
-    password : '2c726724',
-    database : 'heroku_4ea95ec74a86030'
+var db = mysql.createPool({
+    connectionLimit: 5,
+    host: 'us-cdbr-iron-east-05.cleardb.net',
+    user: 'b4a8e285a022ee',
+    password: '2c726724',
+    database: 'heroku_4ea95ec74a86030'
   }); 
   
-db.connect((err) => {
-    if (err) throw err;
-    console.log('MySQL Connected');
+// db.connect((err) => {
+//     if (err) {
+//         console.log("Connection error");
+//         throw err;
+//     }
+//     else {
+//         console.log('MySQL Connected');
+//     }
+// });
+
+db.getConnection((err, connection) => {
+    
 });
 
 router.get('/sql', function(req, res) {
     var q = 'select * from testtable';
     db.query(q, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send(result);
+        if (err) {
+            console.log("Query returned error");
+            throw err;
+        }
+        else {
+            console.log(result);
+            res.send(result);
+        }
     });
 });
+
+// db.end();
 
 module.exports = router;
 
