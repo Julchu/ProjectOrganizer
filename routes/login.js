28a34f4df0ee1ae3a01e71512e2d5dde81723d05
Julian <jchumtl@gmail.com>
2017-09-28 11:20:03 -0400

    Implemented POST for signup

--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var express = require('express');
 var path = require('path');
 
@@ -8,10 +10,27 @@ router.get('/', function(req, res, next) {
 });
 
 router.post("/", function (req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-    console.log("User '" + username + "' has attempted to login with password '" + password + "'.");
-    res.redirect('/project')
+    var user = req.body.user;
+    var pass = req.body.pass;
+    var newUser = req.body.newUser;
+    var newPass = req.body.newPass;
+    var newPassConfirm = req.body.newPassConfirm;
+    var newEmail = req.body.email;
+    if (user && pass) {
+        console.log("User '" + user + "' has attempted to login with password '" + pass + "'.");
+        res.redirect('/project')
+    }
+    else if (newUser && newPass && newPass == newPassConfirm && newEmail){
+        console.log("New user '" + newUser + "' has signed up with email '" + newEmail + ", password '" + newPass + "', and confirmation password '" + newPassConfirm + "'.");
+        res.redirect('/');
+    }
+    else if (newPass && newPass != newPassConfirm) {
+        console.log("Passwords don't match");
+    }
+    else {
+        console.log("Missing information");
+        res.redirect('/');
+    }
 });
 
 module.exports = router;
