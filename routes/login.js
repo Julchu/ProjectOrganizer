4c1669df7e7a2fb668e60fb3ff17cc9c5f2c57bb
Julian <jchumtl@gmail.com>
2017-09-29 20:58:14 -0400

    Trying to implement user validation

--- routes/login.js
+++ routes/login.js
@@ -10,8 +10,12 @@ router.get('/', function(req, res, next) {
     res.sendFile(path.join(__dirname, '../views/login.html'));
 });
 
+sql.validateUser("a");
+// sql.createUser("b", "b@gmail.com", "b");
+// sql.deleteUser("Julian");
+
 router.post("/login", function (req, res) {
-    console.log(sql.validateUser("julian"));
+    
     var user = req.body.user;
     var pass = req.body.pass;
     
@@ -21,21 +25,15 @@ router.post("/login", function (req, res) {
     var newEmail = req.body.email;
     
     if (user && pass) {
-        console.log();
         console.log("User '" + user + "' has successfully logged with password '" + pass + "'.");
-        console.log();
         res.redirect('/project')
     }
     else if (newUser && newPass && newPass == newPassConfirm && newEmail){
-        console.log();
         console.log("New user '" + newUser + "' has signed up with email '" + newEmail + ", password '" + newPass + "', and confirmation password '" + newPassConfirm + "'.");
-        console.log();
         res.redirect('/');
     }
     else if (newPass && newPass != newPassConfirm) {
-        console.log();
         console.log("Passwords don't match");
-        console.log();
     }
     else {
         console.log("Missing information");
--- routes/sql.js
+++ routes/sql.js
@@ -25,42 +25,50 @@ db.getConnection((err, connection) => {
 });
 
 var createUser = function(username, email, password) {
-    var q = 'insert into username' + username;
+    var q = "insert into users values ('" + username + "', '" + email + "', '" + password + "')";
     db.query(q);
+    console.log("create user " + username);
 };
 
 var deleteUser = function(username) {
-    var q = "DELETE FROM users WHERE username=`" + username + "`;";
+    var q = "DELETE FROM users WHERE username='" + username + "';";
     db.query(q);
+    console.log("delete user " + username);
 };
 
 var validateUser = function(username) {
-    var q = "select username='" + username + "' from users";
-    db.query(q, (err, result) => {
-        if (err) {
-            console.log("Query returned error");
-            throw err;
-        }
-        else {
-            console.log(result);
-            return result;
-            // var str = "";
-            // for (var i in result) {
-            //     str = result[i];
-                // if (str.indexOf('julian') !== -1) {
-                //     console.log(str);
-                // }
-                // console.log(str);
-            // }
-            // console.log(str);
-            // str.indexOf("julian");
-            // console.log(str.indexOf("julian"));
-        }
+    // var q = "select 1 from users where username='julian'";
+    // var q = "select username='" + username + "' from users";
+    var q = "select * from users where username='a'";
+    // console.log(db.query(q)._results);
+    var banana = db.query(q, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        // return result;
     });
+    console.log(banana);
+    // console.log("validating user " + username);
+    /*
+    return abcd;
+    */
 };
 
-
 module.exports = {router, createUser, deleteUser, validateUser};
+
+// return result;
+// var str = "";
+// for (var i in result) {
+//     str = result[i];
+    // if (str.indexOf('julian') !== -1) {
+    //     console.log(str);
+    // }
+    // console.log(str);
+// }
+// console.log(str);
+// str.indexOf("julian");
+// console.log(str.indexOf("julian"));
+
+// return result;
 
 //Receiving credentials and authorizing users using passport.js: http://passportjs.org/docs
 // router.post('/login',
